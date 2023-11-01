7200db367e9d90cfd1c9d7554ba10efb9657e94a
Luna <imlvnaa@gmail.com>
2023-11-01 18:59:06 -0400

    Owner role

--- src/commands.ts
+++ src/commands.ts
@@ -15,7 +15,7 @@ import {
 import bot from ".";
 import { readdirSync } from "fs";
 import { join } from "path";
-import { OWNERS } from "./constants";
+import { OWNER_ROLE } from "./constants";
 
 export const prefixCommands: PrefixCommand[] = [];
 export const slashCommands = new Collection<
@@ -94,7 +94,7 @@ async function possiblyTriggerPrefixCommand(
     return !!alias;
   });
   if (!command || !chosenAlias) return;
-  if (command.ownerOnly && !OWNERS.includes(message.author.id)) return;
+  if (command.ownerOnly && !message.member?.roles.cache.has(OWNER_ROLE)) return;
 
   for (let i = 0; i < chosenAlias.split(/ +/g).length; i++) args.shift();
 
