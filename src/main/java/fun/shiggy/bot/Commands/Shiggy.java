11b75511c20adfb7e95d4096f32c6be4117a08af
exhq <echo@nea.moe>
2023-12-07 12:50:34 +0330

    no more token

---
+++ src/main/java/fun/shiggy/bot/Commands/Shiggy.java
@@ -0,0 +1,49 @@
+package fun.shiggy.bot.Commands;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import discord4j.core.object.entity.Message;
+import fun.shiggy.bot.Command;
+import reactor.core.publisher.Mono;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+public class Shiggy implements Command {
+    String randomShiggyValue = "";
+
+    @Override
+    public Mono<Void> execute(Message message) {
+        try {
+            String apiUrl = "https://shiggy.fun/api/v3/shiggies";
+            HttpURLConnection connection = (HttpURLConnection) new URL(apiUrl).openConnection();
+            connection.setRequestMethod("GET");
+
+            JsonArray shiggiesArray =
+                    JsonParser.parseReader(new InputStreamReader(connection.getInputStream())).getAsJsonArray();
+            if (!shiggiesArray.isJsonArray()) {
+                throw new RuntimeException("Unexpected JSON format");
+            }
+
+            int randomIndex = new Random().nextInt(shiggiesArray.size());
+            JsonElement randomShiggy = shiggiesArray.get(randomIndex);
+
+            randomShiggyValue = apiUrl + "/" + randomShiggy.getAsString();
+
+
+        } catch (Exception e) {
+            randomShiggyValue = String.valueOf(e);
+        }
+
+
+        return message.getChannel()
+                .flatMap(messageChannel -> messageChannel.createMessage(randomShiggyValue)
+                        .withMessageReference(message.getId()))
+                .then();
+    }
+
+
+}
